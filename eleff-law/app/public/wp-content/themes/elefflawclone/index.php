4b900c304e28b66a488e3642ecfa8ebe49fce111
Kmerge Boi <kpdosdos@gmail.com>
2023-10-19 00:18:49 +0800

    add text styles

--- eleff-law/app/public/wp-content/themes/elefflawclone/index.php
+++ eleff-law/app/public/wp-content/themes/elefflawclone/index.php
@@ -11,7 +11,7 @@ $parentPageUrl = get_permalink($parentPageId);
   <div class="page-banner__content">
     <img class="page-banner__content__img" src="<?php echo get_template_directory_uri() . '/images/attorney-1.png'; ?>" alt="<?php echo get_bloginfo('name'); ?>">
     <div class="page-banner__content__text">
-      <h3>
+      <h3 class="heading">
         Skilled Legal Counsel <br>
         <span>WITH PROVEN RESULTS</span>
       </h3>
--- eleff-law/app/public/wp-content/themes/elefflawclone/page.php
+++ eleff-law/app/public/wp-content/themes/elefflawclone/page.php
@@ -14,7 +14,7 @@ while (have_posts()) {
     <div class="page-banner__content">
       <img class="page-banner__content__img" src="<?php echo get_template_directory_uri() . '/images/attorney-1.png'; ?>" alt="<?php echo get_bloginfo('name'); ?>">
       <div class="page-banner__content__text">
-        <h3>
+        <h3 class="heading">
           Skilled Legal Counsel <br>
           <span>WITH PROVEN RESULTS</span>
         </h3>
