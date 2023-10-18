a1c70ec178833cd006f31f0428e5319976a9d1e4
Kmerge Boi <kpdosdos@gmail.com>
2023-10-19 00:54:05 +0800

    breadcrumb styles

--- eleff-law/app/public/wp-content/themes/elefflawclone/build/index.js
+++ eleff-law/app/public/wp-content/themes/elefflawclone/build/index.js
@@ -4,3 +4,13 @@ const menuItems = document.querySelectorAll(".menu-item-has-children > a");
 menuItems.forEach((item) => {
   item.innerHTML += chevronDownElement;
 });
+
+// add chevrons (right) icon to breadcrumb
+const chevronsRightElement =
+  '<i class="fas fa-chevron-right"></i><i class="fas fa-chevron-right"></i>';
+const chevronItems = document.querySelectorAll(".breadcrumb > p > span");
+chevronItems.forEach((item, index, items) => {
+  if (items.item(index + 1)) {
+    item.innerHTML += chevronsRightElement;
+  }
+});
