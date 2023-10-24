c63414f1a8e9dd8807a798663282e1f46843bd9c
Kmerge Boi <kpdosdos@gmail.com>
2023-10-24 23:55:52 +0800

    feat: responsive generic page banner

--- eleff-law/app/public/wp-content/themes/elefflawclone/components/page-banner.php
+++ eleff-law/app/public/wp-content/themes/elefflawclone/components/page-banner.php
@@ -1,5 +1,4 @@
 <div class="page-banner">
-  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/banner-background1.jpg'); ?>)"></div>
   <div class="page-banner__content">
     <img class="page-banner__content__img" src="<?php echo get_template_directory_uri() . '/images/attorney-1.png'; ?>" alt="<?php echo get_bloginfo('name'); ?>">
     <div class="page-banner__content__text">
