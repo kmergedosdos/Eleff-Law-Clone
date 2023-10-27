bab3a10427a536f115854871947f1e3af9d854af
Kmerge Boi <kpdosdos@gmail.com>
2023-10-27 16:13:44 +0800

    feat: footer contact form

---
+++ eleff-law/app/public/wp-content/themes/elefflawclone/components/footer-contact-form.php
@@ -0,0 +1,43 @@
+<div class="footer-contact-form">
+  <h2 class="heading">
+    Receive The Practical And Proactive Legal Guidance
+    <span>You Deserve</span>
+  </h2>
+  <form action="" class="form">
+    <p>
+      Fields marked with an&nbsp;<span class="required">*</span>&nbsp;are required
+    </p>
+    <div class="form-field-group">
+      <div class="form-field">
+        <label for="name">Name</label>
+        <input type="text" id="name" name="name">
+      </div>
+      <div class="form-field">
+        <label for="email">Email Address<span class="required">*</span></label>
+        <input type="email" id="email" name="email" required>
+      </div>
+      <div class="form-field">
+        <label for="phone">Phone</label>
+        <input type="tel" id="phone" name="phone">
+      </div>
+    </div>
+    <div class="form-field">
+      <label for="description">Brief description of your legal issue</label>
+      <textarea name="description" id="description" rows="6"></textarea>
+    </div>
+    <div class="actions">
+      <div>
+        <div class="links">
+          <a href="/disclaimer/" id="showDisclaimer">Disclaimer</a>&nbsp;|&nbsp;<a href="/privacy/">Privacy Policy</a>
+        </div>
+        <div class="disclaimer">
+          Disclaimer: The use of the internet or this form for communication with the firm or any individual member of the firm does not establish an attorney-client relationship. Confidential or time-sensitive information should not be sent through this form.
+          <button type="button" id="disclaimer__close" class="disclaimer__button--close">
+            <i class="fa-solid fa-xmark"></i>
+          </button>
+        </div>
+      </div>
+      <button type="submit" class="button button--submit">Schedule Your Free Consultation Today</button>
+    </div>
+  </form>
+</div>
--- eleff-law/app/public/wp-content/themes/elefflawclone/footer.php
+++ eleff-law/app/public/wp-content/themes/elefflawclone/footer.php
@@ -1,6 +1,83 @@
 </main>
 <footer>
-  Footer
+  <?php
+  if (!is_page('contact')) {
+    include('components/footer-contact-form.php');
+  }
+  ?>
+  <div class="footer-offices" style="background-repeat: repeat;background-image: linear-gradient(rgba(254, 254, 254, .95), rgba(254, 254, 254, .99)), url('<?php bloginfo('template_url'); ?>/images/contact-offices-bg.png');">
+    <div class="footer-offices-header">
+      <h2>Contact Our Offices</h2>
+    </div>
+    <div class="footer-offices-description">
+      <div class="office1">
+        <img class="location-icon" src="<?php bloginfo('template_url'); ?>/images/map-pin.png" alt="">
+        <h3>Bethesda Office:</h3>
+        <span class="address">
+          7315 Wisconsin Ave., Suite 400 West <br>
+          Bethesda, MD 20814
+        </span>
+        <span class="location-link">
+          <a href="">Bethesda Office Location</a>
+        </span>
+      </div>
+      <div class="office2">
+        <img class="location-icon" src="<?php bloginfo('template_url'); ?>/images/map-pin.png" alt="">
+        <h3>Silver Spring Office:</h3>
+        <span class="address">
+          12305 Kemp Mill Road <br>
+          Suite B <br>
+          Silver Spring, MD 20902
+        </span>
+        <span class="location-link">
+          <a href="">Silver Spring Office Location</a>
+        </span>
+      </div>
+      <div class="office3">
+        <img class="location-icon" src="<?php bloginfo('template_url'); ?>/images/map-pin.png" alt="">
+        <h3>Washington Office:</h3>
+        <span class="address">
+          5028 Wisconsin Avenue NW, Suite 100 <br>
+          Washington, DC 20016
+        </span>
+        <span class="location-link">
+          <a href="">Washington Office Location</a>
+        </span>
+      </div>
+    </div>
+    <div class="footer-offices-bottom">
+      <div class="phone-icon">
+        <a href=""><i class="icon"></i></a>
+      </div>
+      <div class="phone-number">
+        <h2>Phone:</h2>
+        <a href="">123-456-7890</a>
+      </div>
+    </div>
+  </div>
+  <div class="footer-social">
+    <div class="social-accounts">
+      <ul>
+        <li class="fb"><a href=""><i class="icon"></i></a></li>
+        <li class="twitter"><a href=""><i class="icon"></i></a></li>
+        <li class="linkedin"><a href=""><i class="icon"></i></a></li>
+      </ul>
+    </div>
+    <div class="btn-review-us">
+      <a href="">Review Us</a>
+    </div>
+    <div class="reserved" style="color: white;">
+      <p style="text-align: center;">© <?php echo date('Y'); ?> <a href="https://lawyers.findlaw.com/profile/view/4846143_1" target="_blank">The Eleff Law Group</a> • All Rights Reserved</p>
+    </div>
+    <div class="disclaimer">
+      <p style="text-align: center;">
+        <a href="https://www.elefflaw.com/disclaimer/">Disclaimer</a>&nbsp;|
+        <a href="https://www.elefflaw.com/site-map/">Site Map</a>&nbsp;|
+        <a href="https://www.elefflaw.com/privacy/">Privacy Policy</a> |&nbsp;<a href="https://www.lawyermarketing.com/" target="_blank">Business Development Solutions</a> by
+        <a href="https://www.findlaw.com/" target="_blank">FindLaw</a>, part of Thomson Reuters
+      </p>
+    </div>
+  </div>
 </footer>
 
 <?php wp_footer(); ?>
